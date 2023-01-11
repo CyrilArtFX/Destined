6ac519ca0336883646a197224c3969432b37f590
Cyril Crozat <ccrozat@artfx.fr>
2023-01-11 15:28:16 +0100

    [added] scene changement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        int scenesOpened = SceneManager.sceneCount;
+        if(scenesOpened > 1)
+        {
+            for(int i = 1; i < scenesOpened; i++)
+            {
+                SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i));
+            }
+        }
+        SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Additive);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+    }
+}
