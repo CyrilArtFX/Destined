c021023415f72f070abc4c60d616a7d5746bd8d0
Cyril Crozat <ccrozat@artfx.fr>
2023-01-12 15:38:16 +0100

    [added] stun immunity animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,140 +1,182 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-	public Vector2 Direction { get; private set; }
+    public Vector2 Direction { get; private set; }
 
-	[SerializeField]
-	private float moveSpeed = 5.0f;
-	[SerializeField]
-	private float maxSpeedReduction = .9f;
-	[SerializeField]
-	private float speedReductionPerItem = 0.15f;
-	[SerializeField]
-	private Player player;
-	[SerializeField]
-	private Animator animator;
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+    [SerializeField]
+    private float maxSpeedReduction = .9f;
+    [SerializeField]
+    private float speedReductionPerItem = 0.15f;
+    [SerializeField]
+    private Player player;
+    [SerializeField]
+    private Animator animator;
 
-	[SerializeField]
-	private ParticleSystem stunParticles;
+    [SerializeField]
+    private ParticleSystem stunParticles;
 
-	private new Rigidbody2D rigidbody;
+    [SerializeField]
+    private SpriteRenderer sr;
 
-	[SerializeField]
-	private Transform throwStart;
+    private new Rigidbody2D rigidbody;
 
-	[SerializeField]
-	private GameObject carrotProjectile;
+    [SerializeField]
+    private Transform throwStart;
 
-	[SerializeField]
-	private float stunImmuneTime;
+    [SerializeField]
+    private GameObject carrotProjectile;
+
+    [Header("Stun Immunity")]
+
+    [SerializeField]
+    private float stunImmuneTime;
+
+    [SerializeField]
+    private AnimationCurve stunImmuneAnimation;
 
 
-	public bool IsStun => stun > 0.0f;
-	public bool IsStunImmune => stunImmune;
+    public bool IsStun => stun > 0.0f;
+    public bool IsStunImmune => stunImmune;
 
-	private bool throwing = false;
-	private float stun = 0.0f;
-	private bool stunImmune;
+    private bool throwing = false;
+    private float stun = 0.0f;
+    private bool stunImmune;
+    private bool siaHide;
+    private float siaHideTime, siaMaxHideTime;
 
-	void Awake()
-	{
-		rigidbody = GetComponent<Rigidbody2D>();
-	}
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
-	public void OnMove( InputAction.CallbackContext ctx )
-	{
-		Direction = ctx.action.ReadValue<Vector2>();
-	
-		animator.SetBool( "IsWalking", Direction != Vector2.zero && stun <= 0.0f );
-	}
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Direction = ctx.action.ReadValue<Vector2>();
 
-	public void OnDrop( InputAction.CallbackContext ctx )
-	{
-		if ( !ctx.action.triggered ) return;
+        animator.SetBool("IsWalking", Direction != Vector2.zero && stun <= 0.0f);
+    }
 
-		player.Inventory.DropLastItem();
-	}
+    public void OnDrop(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.action.triggered) return;
 
-	public void OnThrowController( InputAction.CallbackContext ctx )
-	{
-		if (player.Inventory.Items.Count == 0) return;
+        player.Inventory.DropLastItem();
+    }
 
-		Vector2 throw_direction = ctx.action.ReadValue<Vector2>();
+    public void OnThrowController(InputAction.CallbackContext ctx)
+    {
+        if (player.Inventory.Items.Count == 0) return;
+
+        Vector2 throw_direction = ctx.action.ReadValue<Vector2>();
 
         if (throw_direction.magnitude >= 1.0f)
-		{
-			if(!throwing)
-			{
-				throwing = true;
-				throw_direction.Normalize();
+        {
+            if (!throwing)
+            {
+                throwing = true;
+                throw_direction.Normalize();
 
-				SpawnProjectile(throw_direction);
-			}
-		}
-		else if(throw_direction.magnitude <= 0.3f)
-		{
-			throwing = false;
-		}
-	}
+                SpawnProjectile(throw_direction);
+            }
+        }
+        else if (throw_direction.magnitude <= 0.3f)
+        {
+            throwing = false;
+        }
+    }
 
-	public void OnThrowMouse( InputAction.CallbackContext ctx )
+    public void OnThrowMouse(InputAction.CallbackContext ctx)
     {
         if (player.Inventory.Items.Count == 0) return;
 
         if (!ctx.action.triggered) return;
 
-		Vector2 mouse_pos = Mouse.current.position.ReadValue();
-		mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
+        Vector2 mouse_pos = Mouse.current.position.ReadValue();
+        mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
 
-		Vector2 throw_direction = mouse_pos - (Vector2)transform.position;
-		throw_direction.Normalize();
+        Vector2 throw_direction = mouse_pos - (Vector2)transform.position;
+        throw_direction.Normalize();
 
-		SpawnProjectile(throw_direction);
+        SpawnProjectile(throw_direction);
     }
 
-	private void SpawnProjectile(Vector2 direction)
+    private void SpawnProjectile(Vector2 direction)
     {
         GameObject projectile = GameObject.Instantiate(carrotProjectile);
-		projectile.transform.position = throwStart.position;
-		projectile.GetComponent<CarrotProjectile>().Initialize(direction, gameObject);
+        projectile.transform.position = throwStart.position;
+        projectile.GetComponent<CarrotProjectile>().Initialize(direction, gameObject);
 
-		player.Inventory.RemoveItem(player.Inventory.Items[0]);
-		player.Inventory.UpdateItemsPositions();
+        player.Inventory.RemoveItem(player.Inventory.Items[0]);
+        player.Inventory.UpdateItemsPositions();
     }
 
-	public void Stun(float stunTime)
-	{
-		if (stunImmune) return;
+    public void Stun(float stunTime)
+    {
+        if (stunImmune) return;
 
-		stun = stunTime;
-		stunParticles.Play();
-		StartCoroutine(StunImmunity());
+        stun = stunTime;
 
-		//  drop all items
-		while ( player.Inventory.ItemsCount > 0 )
-			player.Inventory.DropLastItem();
-	}
+        var particleMain = stunParticles.main;
+        particleMain.startLifetime = stunTime;
+        stunParticles.Play();
 
-	void Update()
-	{
-		if(stun > 0.0f)
-		{
-			stun -= Time.deltaTime;
-			return;
-		}
+        StartCoroutine(StunImmunity());
 
-		float speed = moveSpeed - moveSpeed * Mathf.Min( maxSpeedReduction, player.Inventory.ItemsCount * speedReductionPerItem );
-		rigidbody.MovePosition( rigidbody.position + Direction * speed );
-	}
+        //  drop all items
+        while (player.Inventory.ItemsCount > 0)
+            player.Inventory.DropLastItem();
+    }
 
-	private IEnumerator StunImmunity()
-	{
-		stunImmune = true;
-		yield return new WaitForSeconds(stunImmuneTime);
-		stunImmune = false;
-	}
+    void Update()
+    {
+        if (stunImmune && siaHideTime > 0.0f)
+        {
+            Color color = sr.color;
+            color.a = Mathf.Lerp( color.a, siaHide ? 1.0f : 0.0f, (siaMaxHideTime - siaHideTime) / siaMaxHideTime );
+            sr.color = color;
+            siaHideTime -= Time.deltaTime;
+        }
+
+        if (stun > 0.0f)
+        {
+            stun -= Time.deltaTime;
+            return;
+        }
+
+        //  everything below this will not happen if the player is stun
+
+        float speed = moveSpeed - moveSpeed * Mathf.Min(maxSpeedReduction, player.Inventory.ItemsCount * speedReductionPerItem);
+        rigidbody.MovePosition(rigidbody.position + Direction * speed);
+    }
+
+    private IEnumerator StunImmunity()
+    {
+        stunImmune = true;
+
+        float timeImmuned = 0.0f;
+        while (timeImmuned < stunImmuneTime)
+        {
+            siaHide = !siaHide;
+            float transitionTime = stunImmuneAnimation.Evaluate(timeImmuned / stunImmuneTime);
+            if (transitionTime + timeImmuned > stunImmuneTime)
+            {
+                transitionTime = stunImmuneTime - timeImmuned;
+            }
+
+            siaHideTime = siaMaxHideTime = transitionTime;
+            //print(transitionTime);
+            timeImmuned += transitionTime;
+            yield return new WaitForSeconds(transitionTime);
+        }
+        siaHide = false;
+
+        stunImmune = false;
+    }
 }
