ea752303347c14f47dcda29b62d39a0bff106ae2
Cyril Crozat <ccrozat@artfx.fr>
2023-01-13 16:58:32 +0100

    [added] visual storage of carrots

--- Assets/Scripts/Storage.cs
+++ Assets/Scripts/Storage.cs
@@ -7,14 +7,27 @@ public class Storage : MonoBehaviour
     private BoxCollider2D boxCollider;
 
     [SerializeField]
-    private TextMeshPro debugText;
+    private Transform playerSpawnPoint;
 
     [SerializeField]
-    private Transform playerSpawnPoint;
+    private BoxCollider2D visualStorageZone;
+
+    [SerializeField]
+    private GameObject visualStoredCarrot;
+
+    [SerializeField]
+    private GameObject visualStoredGoldenCarrot;
 
 
     private Player assignedPlayer;
     private int carrotStored = 0;
+
+
+
+    private float vsMinX;
+    private float vsMaxX;
+    private float vsMinY;
+    private float vsMaxY;
 
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -23,13 +36,12 @@ public class Storage : MonoBehaviour
 
         assignedPlayer.Controller.SetInsideSafeZone(true);
 
-        foreach ( Collectible item in assignedPlayer.Inventory.Items )
-        { 
+        foreach (Collectible item in assignedPlayer.Inventory.Items)
+        {
             carrotStored += item.Score;
+            SpawnVisualStoredCarrot(item.Type == Collectible.ItemType.GOLD_CARROT);
         }
         assignedPlayer.Inventory.ClearInventory();
-
-        debugText.text = carrotStored.ToString();
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -57,5 +69,24 @@ public class Storage : MonoBehaviour
     public int GetPlayerScore()
     {
         return carrotStored;
+    }
+
+    void Start()
+    {
+        vsMinX = visualStorageZone.bounds.center.x - visualStorageZone.bounds.extents.x;
+        vsMaxX = visualStorageZone.bounds.center.x + visualStorageZone.bounds.extents.x;
+        vsMinY = visualStorageZone.bounds.center.y - visualStorageZone.bounds.extents.y;
+        vsMaxY = visualStorageZone.bounds.center.y + visualStorageZone.bounds.extents.y;
+    }
+
+    private void SpawnVisualStoredCarrot(bool spawnGolden)
+    {
+        float rdmX = Random.Range(vsMinX, vsMaxX);
+        float rdmY = Random.Range(vsMinY, vsMaxY);
+
+        print(rdmX + "   " + vsMinX + "   " + vsMaxX);
+
+        GameObject carrot = GameObject.Instantiate(spawnGolden ? visualStoredGoldenCarrot : visualStoredCarrot, visualStorageZone.transform, true);
+        carrot.transform.position = new Vector2(rdmX, rdmY);
     }
 }
