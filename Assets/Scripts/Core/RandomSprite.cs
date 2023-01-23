9b590efa15419578916865d4fb84b5cc5ca492ac
Guthen <114919245+arkaht@users.noreply.github.com>
2023-01-23 23:59:58 +0100

    [added] random sprite component

---
+++ Assets/Scripts/Core/RandomSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class RandomSprite : MonoBehaviour
+	{
+		[SerializeField]
+		private Sprite[] sprites;
+
+		private new SpriteRenderer renderer;
+
+		void Awake()
+		{
+			if (sprites.Length > 0)
+			{
+				renderer = GetComponent<SpriteRenderer>();
+				renderer.sprite = sprites[Random.Range(0, sprites.Length)]; 
+			}
+
+			Destroy(this);
+		}
+	}
+}
